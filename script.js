f5c6b59838ef474cacc0fe697f743b9063ac57c3
Shisui <37116322+Shisui6@users.noreply.github.com>
2023-02-09 17:45:33 +0100

    Implement PR changes

--- script.js
+++ script.js
@@ -2,22 +2,20 @@
 
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu-id');
-const closeButton = document.getElementById('close');
 
 // Create event listener functions
-function showMenu() {
+const showMenu = () => {
   menu.classList.replace('hide', 'show');
-}
+};
 
-function hideMenu() {
+const hideMenu = () => {
   menu.classList.replace('show', 'hide');
-}
+};
 
 // Add event listeners to html elements
 menuButton.addEventListener('click', showMenu);
-closeButton.addEventListener('click', hideMenu);
 
-// const menuItems = document.getElementsByClassName('menu-item');
-// for (let i = 0; i < menuItems.length; i += 1) {
-//   menuItems[i].addEventListener('click', hideMenu);
-// }
+const menuItems = document.getElementsByClassName('menu-item');
+for (let i = 0; i < menuItems.length; i += 1) {
+  menuItems[i].addEventListener('click', hideMenu);
+}
